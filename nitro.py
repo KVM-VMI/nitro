61ccd54072ec258ccc949a3c210b106949d7f896
Mathieu Tarral <mathieu.tarral@gmail.com>
2016-06-17 23:24:13 +0200

    add Nitro class

--- nitro.py
+++ nitro.py
@@ -22,6 +22,47 @@ KVM_NITRO_EVENT_ERROR = 1
 KVM_NITRO_EVENT_SYSCALL = 2
 KVM_NITRO_EVENT_SYSRET = 3
 
+
+class Nitro:
+
+    def __init__(self, pid):
+        self.pid = pid
+        logging.debug('Loading libnitro.so')
+        self.libnitro = cdll.LoadLibrary('./libnitro/libnitro.so')
+
+
+    def __enter__(self):
+        logging.debug('Initializing KVM')
+        self.libnitro.init_kvm()
+        logging.debug('Attaching to the VM')
+        self.libnitro.attach_vm(c_int(self.pid))
+        logging.debug('Attaching to VCPUs')
+        self.libnitro.attach_vcpus()
+        logging.debug('Setting Traps')
+        self.libnitro.set_syscall_trap(True)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        logging.debug('Unsetting Traps')
+        self.libnitro.set_syscall_trap(False)
+        logging.debug('Closing KVM')
+        self.libnitro.close_kvm()
+
+
+    def listen(self):
+        while 1:
+            try:
+                event = self.libnitro.get_event(0)
+                if event == KVM_NITRO_EVENT_SYSCALL:
+                    logging.debug('SYSCALL')
+                elif event == KVM_NITRO_EVENT_SYSRET:
+                    logging.debug('SYSRET')
+
+                yield(event)
+                self.libnitro.continue_vm(0)
+            except KeyboardInterrupt:
+                break
+
 def init_logger():
     logger = logging.getLogger()
     logger.addHandler(logging.StreamHandler())
@@ -30,31 +71,10 @@ def init_logger():
 def main(args):
     pid = int(args['<pid>'])
     logging.debug('pid = {}'.format(pid))
-    logging.debug('Loading libnitro.so')
-    libnitro = cdll.LoadLibrary('./libnitro/libnitro.so')
-    logging.debug('Initializing KVM')
-    fd = libnitro.init_kvm()
-    logging.debug('Attaching to the VM')
-    vmfd = libnitro.attach_vm(c_int(pid))
-    logging.debug('Attaching to VCPUs')
-    nb_vcpus = libnitro.attach_vcpus()
-    logging.debug('Setting Traps')
-    libnitro.set_syscall_trap(True)
-    while 1:
-        try:
-            event = libnitro.get_event(0)
-            if event == KVM_NITRO_EVENT_SYSCALL:
-                logging.debug('SYSCALL')
-            elif event == KVM_NITRO_EVENT_SYSRET:
-                logging.debug('SYSRET')
 
-            libnitro.continue_vm(0)
-        except KeyboardInterrupt:
-            logging.debug('Unsetting Traps')
-            libnitro.set_syscall_trap(False)
-            logging.debug('Closing KVM')
-            libnitro.close_kvm()
-            break
+    with Nitro(pid) as nitro:
+        for event in nitro.listen():
+            logging.debug(event)
 
 if __name__ == '__main__':
     init_logger()
