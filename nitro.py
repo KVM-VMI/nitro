a9d44876509b0fdd8fa6f89900e8cfbb33dd8d93
Mathieu Tarral <mathieu.tarral@gmail.com>
2016-06-17 23:49:21 +0200

    add class Regs

--- nitro.py
+++ nitro.py
@@ -18,6 +18,28 @@ from ctypes import *
 
 from docopt import docopt
 
+class Regs(Structure):
+    _fields_ = [
+                ('rax', c_ulonglong),
+                ('rbx', c_ulonglong),
+                ('rcx', c_ulonglong),
+                ('rdx', c_ulonglong),
+                ('rsi', c_ulonglong),
+                ('rdi', c_ulonglong),
+                ('rsp', c_ulonglong),
+                ('rbp', c_ulonglong),
+                ('r8', c_ulonglong),
+                ('r9', c_ulonglong),
+                ('r10', c_ulonglong),
+                ('r11', c_ulonglong),
+                ('r12', c_ulonglong),
+                ('r13', c_ulonglong),
+                ('r14', c_ulonglong),
+                ('r15', c_ulonglong),
+                ('rip', c_ulonglong),
+                ('rflags', c_ulonglong),
+            ]
+
 
 class Event:
 
@@ -25,14 +47,15 @@ class Event:
     KVM_NITRO_EVENT_SYSCALL = 2
     KVM_NITRO_EVENT_SYSRET = 3
 
-    def __init__(self, event_type):
+    def __init__(self, event_type, regs):
         if event_type == self.KVM_NITRO_EVENT_ERROR:
             raise RuntimeError()
         self.event_type = event_type
+        self.regs = regs
 
     def __str__(self):
         if self.event_type == self.KVM_NITRO_EVENT_SYSCALL:
-            return "SYSCALL"
+            return "SYSCALL rax = {}".format(hex(self.regs.rax))
         else:
             return "SYSRET"
 
@@ -68,7 +91,10 @@ class Nitro:
         while 1:
             try:
                 event = self.libnitro.get_event(0)
-                e = Event(event)
+                regs = Regs()
+                self.libnitro.get_regs(0, byref(regs))
+
+                e = Event(event, regs)
 
                 yield(e)
                 self.libnitro.continue_vm(0)
