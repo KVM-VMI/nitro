902e8770da427d16eac3c93be8600fa36095e09f
Mathieu Tarral <mathieu.tarral@gmail.com>
2017-01-13 11:03:55 +0200

    move main in nitro.py

--- nitro.py
+++ nitro.py
@@ -1,88 +1,30 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """Nitro.
 
 Usage:
-  nitro.py [options] <vm_name> (32 | 64)
+  nitro.py [options] <vm_name>
 
 Options:
   -h --help     Show this screen.
-  --nobackend   Dont analyze events
 
 """
 
-
-import os
-import sys
-import re
-import struct
 import logging
-import subprocess
+import signal
 import json
-from pprint import pprint
-from ctypes import *
-
-from docopt import docopt
 import libvirt
+from docopt import docopt
 
-from event import Event, Regs, SRegs, NitroEvent
-from backend import Backend
+from libnitro import Nitro
 
+run = True
+# def new signal for SIGINT
+def sigint_handler(signal, frame):
+    global run
+    run = False
+signal.signal(signal.SIGINT, sigint_handler)
 
-class Nitro:
-
-    def __init__(self, pid, vm_name):
-        self.pid = pid
-        logging.debug('Loading libnitro.so')
-        script_dir = os.path.dirname(os.path.realpath(__file__))
-        libnitro_so_path = os.path.join(script_dir, 'libnitro', 'libnitro.so')
-        self.libnitro = cdll.LoadLibrary(libnitro_so_path)
-        con = libvirt.open('qemu:///system')
-        self.domain = con.lookupByName(vm_name)
-
-
-    def __enter__(self):
-        logging.debug('Suspending the Guest')
-        self.domain.suspend()
-        logging.debug('Initializing KVM')
-        self.libnitro.init_kvm()
-        logging.debug('Attaching to the VM')
-        self.libnitro.attach_vm(c_int(self.pid))
-        logging.debug('Attaching to VCPUs')
-        self.libnitro.attach_vcpus()
-        logging.debug('Setting Traps')
-        self.libnitro.set_syscall_trap(True)
-        logging.debug('Resuming the Guest')
-        self.domain.resume()
-        return self
-
-    def __exit__(self, type, value, traceback):
-        logging.debug('Suspending the Guest')
-        self.domain.suspend()
-        logging.debug('Unsetting Traps')
-        self.libnitro.set_syscall_trap(False)
-        logging.debug('Closing KVM')
-        self.libnitro.close_kvm()
-        logging.debug('Resuming the Guest')
-        self.domain.resume()
-
-
-    def listen(self):
-        while 1:
-            try:
-                nitro_ev = NitroEvent()
-                self.libnitro.get_event(0, byref(nitro_ev))
-                regs = Regs()
-                sregs = SRegs()
-                self.libnitro.get_regs(0, byref(regs))
-                self.libnitro.get_sregs(0, byref(sregs))
-
-                e = Event(nitro_ev, regs, sregs)
-
-                yield(e)
-                self.libnitro.continue_vm(0)
-            except KeyboardInterrupt:
-                break
 
 def init_logger():
     logger = logging.getLogger()
@@ -91,23 +33,26 @@ def init_logger():
 
 def main(args):
     vm_name = args['<vm_name>']
-    if args['32']:
-        arch = 32
-    else:
-        arch = 64
-    logging.debug('Finding PID of VM {}'.format(vm_name))
-    output = subprocess.check_output("pgrep -f -o 'qemu.*-name {}'".format(vm_name), shell=True)
-    pid = int(output)
-    logging.debug('pid = {}'.format(pid))
+    # get domain from libvirt
+    con = libvirt.open('qemu:///system')
+    domain = con.lookupByName(vm_name)
 
-    if not args['--nobackend']:
-        backend = Backend(vm_name, arch)
-    with Nitro(pid, vm_name) as nitro:
+    counter = 0
+    events = []
+    # start Nitro
+    with Nitro(domain) as nitro:
         for event in nitro.listen():
-            if not args['--nobackend']:
-                backend.new_event(event)
-            else:
-                logging.debug(event.display())
+            # get event dict
+            event_info = event.info()
+            events.append(event_info)
+            # wait for CTRL+C to stop
+            if not run:
+                break
+    logging.info('Writing events')
+    with open('events.json', 'w') as f:
+        json.dump(events, f)
+
+
 
 if __name__ == '__main__':
     init_logger()
