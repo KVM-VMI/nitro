01de86e68accc49df07da0d03661a40f77385bd5
Mathieu Tarral <mathieu.tarral@f-secure.com>
2017-01-26 11:43:16 +0200

    update test_nitro to use a CDROM instead of WinRM

--- tests/test_nitro.py
+++ tests/test_nitro.py
@@ -5,23 +5,14 @@ import sys
 import re
 import logging
 import subprocess
+import shutil
 import time
 import xml.etree.ElementTree as tree
 import threading
+from tempfile import TemporaryDirectory, NamedTemporaryFile
 from datetime import timedelta
 
 import libvirt
-import winrm
-
-# fix timeout
-import requests
-old_req_send = requests.Session.send
-
-# force a fixed timeout value
-def send_fix_timeout(self, request, **kwargs):
-    return old_req_send(self, request, timeout=3000)
-# patch
-requests.Session.send = send_fix_timeout
 
 # add parent directory
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
@@ -30,6 +21,12 @@ from nitro.event import SyscallDirection
 
 
 NB_TEST = 3
+
+def clear_arp_cache():
+    logging.info('Flushing Arp cache')
+    subprocess.check_call(['ip', '-s', '-s', 'neigh', 'flush', 'all'],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL)
 
 def get_ip(mac_addr):
     while True:
@@ -42,96 +39,108 @@ def get_ip(mac_addr):
         time.sleep(1)
 
 
-def start_stop(func):
-    def wrapper(domain):
-        # start domain
-        logging.info('Testing {}'.format(domain.name()))
-        domain.create()
-        func(domain)
-        # shutdown
-        domain.shutdown()
-        logging.info('Waiting for shutdown')
-        while domain.state()[0] != libvirt.VIR_DOMAIN_SHUTOFF:
-            time.sleep(1)
-    return wrapper
+def run_nitro_thread(domain, stop_request):
+    nb_syscalls = 0
+    with Nitro(domain) as nitro:
+        logging.info('Counting syscalls...')
+        for event in nitro.listen():
+            if event.direction == SyscallDirection.enter:
+                nb_syscalls += 1
+            if stop_request.isSet():
+                break
+    logging.info('Nb Syscalls : {}'.format(nb_syscalls))
+
+def get_test_content():
+    content = """
+powershell -Command \"Get-ChildItem -Path C:\\windows"
+"""[1:].replace('\n', '\r\n')
+    return content
 
 
-def run_nitro(func):
-    def wrapper(domain, session):
-
-        stop_request = threading.Event()
-        def run_nitro_thread(stop_request):
-            nb_syscalls = 0
-            with Nitro(domain) as nitro:
-                logging.info('Counting syscalls...')
-                for event in nitro.listen():
-                    if event.direction == SyscallDirection.enter:
-                        nb_syscalls += 1
-                    if stop_request.isSet():
-                        break
-            logging.info('Nb Syscalls : {}'.format(nb_syscalls))
-
-        # start thread
-        thread = threading.Thread(target=run_nitro_thread, args=(stop_request,))
-        thread.start()
-
-        result = func(domain, session)
-
-        # wait for thread to stop
-        stop_request.set()
-        thread.join()
-        return result
-    return wrapper
-
-
-@run_nitro
-def run_test(domain, session):
-    logging.info('Running test command')
-    # command that will be executed in user desktop session
-    exe = "c:\\windows\\system32\\WindowsPowerShell\\v1.0\\powershell.exe"
-    args = ["-Command", "Get-ChildItem -Path C:\\windows"]
-    
-    # prepare psexec command
-    args_psexec_display = ["-accepteula", "-s", "-i", "1"]
-    args_psexec = args_psexec_display
-    args_psexec.append(exe)
-    args_psexec.extend(args)
-    logging.debug('Executing PowerShell command : {}'.format(args[1]))
-    start = None
-    end = None
-    while True:
-        try:
-            start = time.time()
-            session.run_cmd('c:\\pstools\\PsExec64.exe', args_psexec)
-            end = time.time()
-        except winrm.exceptions.WinRMTransportError:
-            logging.debug('WinRM error, retrying')
-        else:
-            break
-    total_seconds = end - start
-    return total_seconds
-
-
-
-@start_stop
 def test_domain(domain):
+    # start domain
+    logging.info('Testing {}'.format(domain.name()))
+    domain.create()
+    # find MAC address
     dom_elem = tree.fromstring(domain.XMLDesc())
     mac_addr = dom_elem.find("./devices/interface[@type='network']/mac").get('address')
     logging.debug('MAC address : {}'.format(mac_addr))
-    # wait for winrm connection
+    # clear ARP cache
+    clear_arp_cache()
+    # wait for IP address
     ip = get_ip(mac_addr)
     logging.info('IP address : {}'.format(ip))
-    logging.info('Establishing a WinRM session')
-    s = winrm.Session(ip, auth=('vagrant', 'vagrant'))
-    s.run_cmd('ipconfig')
+    tmp_iso = build_cdrom()
+    # run nitro
+    stop_request = threading.Event()
+    thread = threading.Thread(target=run_nitro_thread, args=(domain, stop_request,))
+    thread.start()
+    # start timer
+    start_time = time.time()
+    # mount cdrom, test is executed
+    mount_cdrom(domain, tmp_iso.name)
+    # wait for shutdown
+    while True:
+        if domain.state()[0] == libvirt.VIR_DOMAIN_SHUTOFF:
+            break
+        time.sleep(1)
+    stop_time = time.time()
+    result = stop_time - start_time
+    # stop nitro and clean
+    stop_request.set()
+    #thread.join()
+    tmp_iso.close()
+    return result
 
-    total = 0
-    for i in range(1,NB_TEST + 1):
-        result = run_test(domain, s)
-        logging.info('[TEST {}] Total execution time : {}'.format(i, timedelta(seconds=result)))
-        total += result
-    avg_total = total / NB_TEST
-    logging.info('Average execution time : {}'.format(timedelta(seconds=avg_total)))
+def mount_cdrom(domain, cdrom_path):
+    logging.info('Mounting CDROM image')
+    dom_elem = tree.fromstring(domain.XMLDesc())
+    # find cdrom
+    cdrom_elem = dom_elem.find("./devices/disk[@device='cdrom']")
+    # find source
+    source_elem = cdrom_elem.find('./source')
+    if source_elem is None:
+        tree.SubElement(cdrom_elem, 'source')
+        source_elem = cdrom_elem.find('./source')
+    source_elem.set('file', cdrom_path)
+    new_xml = tree.tostring(cdrom_elem).decode('utf-8')
+    domain.updateDeviceFlags(new_xml, libvirt.VIR_DOMAIN_AFFECT_LIVE)
+
+
+def build_cdrom():
+    tmp_iso = NamedTemporaryFile(delete=False)
+    # create tmp dir
+    with TemporaryDirectory() as tmpdir:
+        fill_cdrom(tmpdir)
+        # generate iso
+        genisoimage_bin = shutil.which('genisoimage')
+        args = [genisoimage_bin, '-o', tmp_iso.name, '-iso-level', '4', tmpdir]
+        subprocess.check_call(args,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL)
+    logging.debug('ISO generated at {}'.format(tmp_iso.name))
+    return tmp_iso
+
+
+
+def fill_cdrom(tmpdir):
+    # write autorun.inf
+    content = """
+[autorun]
+open=run.bat
+"""[1:].replace('\n', '\r\n')
+    autorun_path = os.path.join(tmpdir, 'autorun.inf')
+    with open(autorun_path, 'w') as f:
+        f.write(content)
+    # write run.bat
+    content = get_test_content()
+    end_test = """
+shutdown -s -f -t 0
+"""[1:].replace('\n', '\r\n')
+    content += end_test
+    run_bat_path = os.path.join(tmpdir, 'run.bat')
+    with open(run_bat_path, 'w') as f:
+        f.write(content)
 
 
 def main():
@@ -142,7 +151,13 @@ def main():
     con = libvirt.open('qemu:///system')
     for domain in con.listAllDomains():
         if re.match('nitro_.*', domain.name()):
-            test_domain(domain)
+            total = 0
+            for i in range(1,NB_TEST + 1):
+                result = test_domain(domain)
+                logging.info('[TEST {}] Total execution time : {}'.format(i, timedelta(seconds=result)))
+                total += result
+            avg_total = total / NB_TEST
+            logging.info('Average execution time : {}'.format(timedelta(seconds=avg_total)))
 
 
 if __name__ == '__main__':
