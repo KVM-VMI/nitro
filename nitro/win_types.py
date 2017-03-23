327ccee631a9d54952178fe07fb1d6df66b7961d
Mathieu Tarral <mathieu.tarral@f-secure.com>
2017-03-23 14:18:24 +0200

    comment logging output

--- nitro/win_types.py
+++ nitro/win_types.py
@@ -7,16 +7,16 @@ class WinStruct(object):
     _fields_ = []
 
     def __init__(self, addr, pid, vmi):
-        logging.debug('Building {} from {}'.format(self.__class__.__name__, hex(addr)))
+        # logging.debug('Building {} from {}'.format(self.__class__.__name__, hex(addr)))
         self.addr = addr
         self.pid = pid
         self.vmi = vmi
         for f_offset, f_name, f_format in self._fields_:
-            logging.debug('Field {}, {}, at {} + {}'.format(f_name, f_format, hex(addr), hex(f_offset)))
+            # logging.debug('Field {}, {}, at {} + {}'.format(f_name, f_format, hex(addr), hex(f_offset)))
             f_size = struct.calcsize(f_format)
             content = self.vmi.read_va(addr + f_offset, self.pid, f_size)
             f_value, *rest = struct.unpack(f_format, content)
-            logging.debug('Value: {}'.format(hex(f_value)))
+            # logging.debug('Value: {}'.format(hex(f_value)))
             setattr(self, f_name, f_value)
 
 
