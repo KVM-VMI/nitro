43f90d7d95444dd79d740ae36e6e1313cd399bce
Mathieu Tarral <mathieu.tarral@f-secure.com>
2017-03-31 11:29:29 +0300

    add test_hook_openkey

--- tests/test_nitro.py
+++ tests/test_nitro.py
@@ -355,5 +355,37 @@ class TestNitro(unittest.TestCase):
             json.dump(events, f, indent=4)
         logging.info('Test execution time {}'.format(exec_time))
         # checking if we find the event where the file is opened
-        event_file_opened = [e for e in events if e.get('hook') and e['hook'].find(file_path)]
-        self.assertTrue(event_file_opened)
+        event_found = [e for e in events if e.get('hook') and e['hook'].find(file_path)]
+        self.assertTrue(event_found)
+
+    def test_hook_openkey(self):
+        key_path = 'Software\\ABCDMagicKey1234'
+        script = 'New-Item \"HKCU:\\{}\" -Force | New-ItemProperty -Name foobar -Value true -PropertyType STRING -Force'.format(key_path)
+        self.cdrom.set_script(script, powershell=True)
+        cdrom_iso = self.cdrom.generate_iso()
+
+        def enter_NtOpenKey(syscall):
+            KeyHandle, DesiredAccess, object_attributes = syscall.collect_args(3)
+            obj = ObjectAttributes(object_attributes, syscall.process)
+            buffer = obj.PUnicodeString.Buffer
+            syscall.hook = buffer
+
+        def enter_NtCreateKey(syscall):
+            KeyHandle, DesiredAccess, object_attributes = syscall.collect_args(3)
+            obj = ObjectAttributes(object_attributes, syscall.process)
+            buffer = obj.PUnicodeString.Buffer
+            syscall.hook = buffer
+
+        hooks = {
+            'NtOpenKey': enter_NtOpenKey,
+            'NtCreateKey': enter_NtCreateKey,
+        }
+        events, exec_time = self.vm_test.run(cdrom_iso, hooks=hooks)
+        # writing events
+        logging.debug('Writing events...')
+        with open('events.json', 'w') as f:
+            json.dump(events, f, indent=4)
+        logging.info('Test execution time {}'.format(exec_time))
+        # checking if we find the event where the file is opened
+        event_found = [e for e in events if e.get('hook') and e['hook'].find(key_path)]
+        self.assertTrue(event_found)
