4846b448155502ac4f608a6a5f28d27cb410b659
Mathieu Tarral <mathieu.tarral@f-secure.com>
2017-06-15 11:48:44 +0300

    remove nitro listener from Backend parameters

--- nitro/backends/windows/backend.py
+++ nitro/backends/windows/backend.py
@@ -25,8 +25,8 @@ class WindowsBackend(Backend):
         "processes"
     )
 
-    def __init__(self, domain, listener, libvmi):
-        super().__init__(domain, listener, libvmi)
+    def __init__(self, domain, libvmi):
+        super().__init__(domain, libvmi)
         vcpus_info = self.domain.vcpus()
         self.nb_vcpu = len(vcpus_info[0])
 
