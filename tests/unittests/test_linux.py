018031bfc41cd0701e9f6b9a1a48c39d88af72c7
Samuel Laurén <samuel.lauren@iki.fi>
2017-09-28 15:22:42 +0300

    Unit test for associating names with system call numbers

--- tests/unittests/test_linux.py
+++ tests/unittests/test_linux.py
@@ -1,8 +1,11 @@
-import sys
 import os
+import pathlib
+import struct
+import sys
 import unittest
+import logging
 
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 # local
 sys.path.insert(1, os.path.realpath('../..'))
@@ -17,11 +20,16 @@ def translate_ksym2v(symbol):
     }[symbol]
 
 def translate_v2ksym(symbol):
-    return {}[symbol]
+    return {
+        18446744071581005472: "SyS_read",
+        18446744071581005664: "SyS_write",
+        18446744071580990032: "SyS_close",
+        18446744071580998512: "SyS_open"
+    }[symbol]
 
 def translate_ksym2v(symbol):
     return {
-        "sys_call_table": 0xc0ffee
+        "sys_call_table": 0xc0ffee,
     }[symbol]
 
 def get_offset(symbol):
@@ -44,6 +52,9 @@ domain = Mock(**{
                   "vcpus.return_value": [[2]]
               })
 
+def get_resource_path(name):
+    return pathlib.Path(__file__).parent.joinpath("resources", name)
+
 class TestLinux(unittest.TestCase):
     def test_backend_creation(self):
         """Check that LinuxBackend can be creted."""
@@ -58,8 +69,17 @@ class TestLinux(unittest.TestCase):
     def test_syscall_name(self):
         """Check that syscall names can be extracted from system call table."""
         backend = LinuxBackend(domain, libvmi)
-
-        # Test syscall table inspection
-        pass
+        with get_resource_path("syscall_table_sample.bin").open("rb") as handle:
+            memory = handle.read()
+        base = translate_ksym2v("sys_call_table")
+        def read_addr_va(addr, pid):
+            start = addr - base
+            return struct.unpack("P", memory[start:start+8])[0]
+        with patch.object(backend.libvmi, "read_addr_va", side_effect=read_addr_va) \
+             as mock_read_addr_va:
+            self.assertEqual(backend.get_syscall_name(0), "SyS_read")
+            self.assertEqual(backend.get_syscall_name(1), "SyS_write")
+            self.assertEqual(backend.get_syscall_name(2), "SyS_open")
+            self.assertEqual(backend.get_syscall_name(3), "SyS_close")
 
 
