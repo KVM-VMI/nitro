03f9cbebc21cc796759091f1be0d449bf7f6569c
Samuel Laurén <samuel.lauren@iki.fi>
2017-10-02 15:40:32 +0300

    Use githubpages Sphinx extension to avoid problems with Jekyll

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,15 +13,6 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- General configuration ------------------------------------------------
 
 # Modify path
@@ -43,7 +34,9 @@ for mod in ("libvirt", "nitro._libvmi", "ioctl_opt", "docopt"):
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
