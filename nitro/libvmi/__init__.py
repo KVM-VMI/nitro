6907287cee00c2d9a4f9c66ae9d5a3c963c89f06
Mathieu Tarral <mathieu.tarral@f-secure.com>
2018-02-20 23:57:38 +0200

    fix paths in libvmi/__init__.py

--- nitro/libvmi/__init__.py
+++ nitro/libvmi/__init__.py
@@ -1,5 +1,5 @@
 # public interface
-from libvmi.libvmi import INIT_DOMAINNAME, INIT_DOMAINID, INIT_EVENTS, INIT_SHM
-from libvmi.libvmi import Libvmi, LibvmiError, VMIConfig
-from libvmi.libvmi import VMIStatus, LibvmiInitError, PageMode
-from libvmi.libvmi import VMIArch, VMIOS, VMIWinVer
+from nitro.libvmi.libvmi import INIT_DOMAINNAME, INIT_DOMAINID, INIT_EVENTS, INIT_SHM
+from nitro.libvmi.libvmi import Libvmi, LibvmiError, VMIConfig
+from nitro.libvmi.libvmi import VMIStatus, LibvmiInitError, PageMode
+from nitro.libvmi.libvmi import VMIArch, VMIOS, VMIWinVer
