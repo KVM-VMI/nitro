cfbda5e2184d4b9d1ab67bd83f6a62d8bf882fdb
Mathieu Tarral <mathieu.tarral@f-secure.com>
2018-02-20 23:35:42 +0200

    refactor libvmi CFFI interface, upgrade to 1.0.0

---
+++ nitro/libvmi/__init__.py
@@ -0,0 +1,5 @@
+# public interface
+from libvmi.libvmi import INIT_DOMAINNAME, INIT_DOMAINID, INIT_EVENTS, INIT_SHM
+from libvmi.libvmi import Libvmi, LibvmiError, VMIConfig
+from libvmi.libvmi import VMIStatus, LibvmiInitError, PageMode
+from libvmi.libvmi import VMIArch, VMIOS, VMIWinVer
