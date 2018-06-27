2502d549dad6492bd775bab5ca15790f9e15b5ee
Mathieu Tarral <mathieu.tarral@f-secure.com>
2018-06-27 16:40:20 +0300

    remove nitro/libvmi

--- nitro/libvmi/__init__.py
+++
@@ -1,5 +0,0 @@
-# public interface
-from nitro.libvmi.libvmi import INIT_DOMAINNAME, INIT_DOMAINID, INIT_EVENTS, INIT_SHM
-from nitro.libvmi.libvmi import Libvmi, LibvmiError, VMIConfig
-from nitro.libvmi.libvmi import VMIStatus, LibvmiInitError, PageMode
-from nitro.libvmi.libvmi import VMIArch, VMIOS, VMIWinVer
